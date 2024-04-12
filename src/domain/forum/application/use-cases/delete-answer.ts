94fac3a709987218963205977da62d5c0a8cca70
pedrodecf <pedro012014@gmail.com>
2024-04-12 12:42:53 -0300

    feat: :sparkles: caso de uso para deletar answer e factory para criar answer

---
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -0,0 +1,31 @@
+import { AnswerRepository } from '../repositories/answer-repository'
+
+interface DeleteAnswerUseCaseRequest {
+  answerId: string
+  authorId: string
+}
+
+interface DeleteAnswerUseCaseResponse {}
+
+export class DeleteAnswerUseCase {
+  constructor(private answerRepository: AnswerRepository) {}
+
+  async execute({
+    answerId,
+    authorId,
+  }: DeleteAnswerUseCaseRequest): Promise<DeleteAnswerUseCaseResponse> {
+    const answer = await this.answerRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found')
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error('You are not the author of this answer')
+    }
+
+    await this.answerRepository.delete(answer)
+
+    return {}
+  }
+}
